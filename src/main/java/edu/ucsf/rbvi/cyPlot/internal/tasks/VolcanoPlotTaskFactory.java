e0889078fb031d29974b0ca557512af82e0d7a81
Scooter Morris <scooter@cgl.ucsf.edu>
2018-07-03 14:44:06 -0700

    Initial commit of some template code

---
+++ src/main/java/edu/ucsf/rbvi/cyPlot/internal/tasks/VolcanoPlotTaskFactory.java
@@ -0,0 +1,25 @@
+package edu.ucsf.rbvi.cyPlot.internal.tasks;
+
+import org.cytoscape.service.util.CyServiceRegistrar;
+import org.cytoscape.work.TaskFactory;
+import org.cytoscape.work.TaskIterator;
+
+public class VolcanoPlotTaskFactory implements TaskFactory {
+	CyServiceRegistrar serviceRegistrar;
+
+	public VolcanoPlotTaskFactory(CyServiceRegistrar reg) {
+		this.serviceRegistrar = reg;
+	}
+
+	public TaskIterator createTaskIterator() { 
+		// This should do something like:
+		// return new TaskIterator(new VolcanoPlotTask(serviceRegistrar);
+		return null;
+	}
+
+	public boolean isReady() { 
+		// Check to see if there are any networks
+		// If so, return true
+		return true; 
+	}
+}
