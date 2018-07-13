fe3f7e173b4d199020f45999ed6b365aba533dfc
liammagee <lpdmagee@gmail.com>
2018-07-13 13:24:34 -0700

    Now auto-generates the range for the plot

--- src/main/java/edu/ucsf/rbvi/cyPlot/internal/tasks/ScatterPlotTask.java
+++ src/main/java/edu/ucsf/rbvi/cyPlot/internal/tasks/ScatterPlotTask.java
@@ -112,7 +112,8 @@ public class ScatterPlotTask extends AbstractTask {
 		//String html5 = "<script> var trace1 = { x: [0, 1, 2, 3, 4, 5, 6, 7, 8], y: [8, 7, 6, 5, 4, 3, 2, 1, 0], type: 'scatter'};";
 		//String html6 = "var trace2 = { x: [0, 1, 2, 3, 4, 5, 6, 7, 8], y: [0, 1, 2, 3, 4, 5, 6, 7, 8], type:'scatter'};";
 		String html7 = "var data = [trace1];";
-		String html8 = "var layout = { xaxis: {range: [0,5]}, yaxis: {range: [0,5]}};";
+		//String html8 = "var layout = { xaxis: {range: [0,5]}, yaxis: {range: [0,5]}};";
+		String html8 = "var layout = {};";
 		String html9 = "Plotly.react('scattertest', data, layout);";
 		String html10 = "</script></body></html>";
 		
