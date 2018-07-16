4800e89a57d86114149b196be61a2fbee51cb407
liammagee <lpdmagee@gmail.com>
2018-07-16 11:27:44 -0700

    Added functioning scatter plot with hover (duplicated ScatterPlotTask)

--- src/main/java/edu/ucsf/rbvi/cyPlot/internal/tasks/VolcanoPlotTask.java
+++ src/main/java/edu/ucsf/rbvi/cyPlot/internal/tasks/VolcanoPlotTask.java
@@ -1,19 +1,15 @@
 package edu.ucsf.rbvi.cyPlot.internal.tasks;
 
+import javax.swing.JFrame;
+
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Properties;
 import java.util.concurrent.Callable;
 
-import javax.swing.JFrame;
-
 import org.cytoscape.application.CyApplicationManager;
-import org.cytoscape.application.events.SetCurrentNetworkEvent;
-import org.cytoscape.application.events.SetCurrentNetworkListener;
 import org.cytoscape.command.AvailableCommands;
 import org.cytoscape.command.CommandExecutorTaskFactory;
 import org.cytoscape.service.util.CyServiceRegistrar;
@@ -27,80 +23,141 @@ import org.cytoscape.work.util.ListSingleSelection;
 import org.cytoscape.model.CyColumn;
 import org.cytoscape.model.CyNetwork;
 import org.cytoscape.model.CyTable;
-import org.cytoscape.model.subnetwork.CyRootNetwork;
 
 public class VolcanoPlotTask extends AbstractTask {
+	
+	final CyServiceRegistrar sr;
+	@Tunable (description="Fold change column")
+	public ListSingleSelection<String> xCol;
+
+	@Tunable (description="P-val column")
+	public ListSingleSelection<String> yCol;
+	
+	@Tunable (description="Markers?")
+	public ListSingleSelection<String> mode;
+	
 	public CyApplicationManager appManager;
 	public CyNetworkView netView;
-	final CyServiceRegistrar sr;
-	
-	@Tunable (description="Fold-change column")
-    public ListSingleSelection<Callable<CyColumn>> foldChangeCol;
-
-     @Tunable (description="P-Value column")
-	public ListSingleSelection<Callable<CyColumn>> pValCol;
+	public CyNetwork network;
+	public CyTable table;
+	public Collection<CyColumn> columns;
 	
 	public VolcanoPlotTask(final CyServiceRegistrar sr) {
 		super();
-		this.sr = sr;
+		this.sr = sr; 
 		appManager = sr.getService(CyApplicationManager.class);
 		netView = appManager.getCurrentNetworkView();
-		CyNetwork network = netView.getModel();
-		CyTable table = network.getDefaultNodeTable();
-		Collection<CyColumn> columns = table.getColumns();
+		network = netView.getModel();
+		table = network.getDefaultNodeTable();
+		columns = table.getColumns();
 		
-		List<String> headers = new ArrayList();
+		List<String> headers = new ArrayList<>();
 		for(CyColumn each : columns) {
-			String header = each.getName();
-			headers.add(header);
+			if(!each.getType().isAssignableFrom(String.class) && 
+					!each.getType().isAssignableFrom(Boolean.class) &&
+					!each.getName().equals(CyNetwork.SUID) && 
+					!each.getName().equals(CyNetwork.SELECTED)) {
+				String header = each.getName();
+				headers.add(header);
+			}
 		}
 		
-		foldChangeCol = new ListSingleSelection(headers);
-		pValCol = new ListSingleSelection(headers);
+		xCol = new ListSingleSelection<>(headers);
+		yCol = new ListSingleSelection<>(headers);
+		mode = new ListSingleSelection<>("lines", "markers");
 	}
 	
-    public Callable<CyColumn> getFoldValueSelection() {
-		return foldChangeCol.getSelectedValue();
+	public String getXSelection() {
+		return xCol.getSelectedValue();
 	}
 	
-	public Callable<CyColumn> getPValueSelection() {
-		return pValCol.getSelectedValue();
-	} 
+	public String getYSelection() {
+		return yCol.getSelectedValue();
+	}
+	
+	public String getModeSelection() {
+		return mode.getSelectedValue();
+	}
 
-	public void run(TaskMonitor monitor) throws Exception { 	
+	public void run(TaskMonitor monitor) { 
+		//SynchronousTaskManager sTM = sr.getService(SynchronousTaskManager.class);
 		TaskManager sTM = sr.getService(TaskManager.class);
 		AvailableCommands ac = sr.getService(AvailableCommands.class);
 		CommandExecutorTaskFactory taskFactory = sr.getService(CommandExecutorTaskFactory.class);
 		
-		String html1 = "<html><head><script src=\"https://cdn.plot.ly/plotly-latest.min.js\"></script></head>";
-		String html4 = "<body><div id=\"myDiv\"></div>";
-		String html5 = "<script>function makeplot(){ Plotly.d3.csv(\"https://raw.githubusercontent.com/plotly/datasets/master/2014_apple_stock.csv\", function(data){ processData(data){);}";
-		String html6 = "function processData(allRows){ var x = [], y = [], std = [];";
-		String html7 = "for (var i = 0; i < allRows.length; i++) { row = allRows[i];";
-		String html8 = "x.push(row['AAPL_x']);";
-		String html9 = "y.push(row['AAPL_y']);}";
-		String html10 = "makePlotly(x,y,std);}";
-		String html11 = "function makePlotly(x,y,std){";
-		String html12 = "var plotDiv = document.getElementById(\"plot\");";
-		String html13 = "var traces = [{x: x, y: y}];";
-		String html14 = "var layout = {yaxis: {fixedrange: true}, xaxis: {fixedrange: true}, title: 'my graph'} Plotly.newPlot('myDiv', traces,layout);}; makeplot();";
-		String html15 = "</script></body></html>";
-        String html = html1 + html4 + html5 + html6 + html7 + html8 + html9 + html10 + html11 + html12 + html13 + html14 + html15;
-	//	String html = "<!DOCTYPE HTML><html><body><a href=\"https://www.google.com/\"></a></body></html>";
-		Map<String, Object> args = new HashMap<>();	
-		args.put("text", html);
+		CyColumn xColumn = table.getColumn(getXSelection());
+		CyColumn yColumn = table.getColumn(getYSelection());
+		CyColumn nameColumn = table.getColumn("shared name");
+		
+		String xArray = "[";
+		List<Object> list1 = xColumn.getValues(xColumn.getType());
+		for(int i = 0; i<list1.size(); i++) {
+			xArray += (""+list1.get(i));
+			if(i != list1.size()-1) {
+				xArray += ", ";
+			}else {
+				xArray += "]"; 
+			}
+		}
+		
+		String yArray = "[";
+		List<Object> list2 = yColumn.getValues(yColumn.getType());
+		for(int i = 0; i<list2.size(); i++) {
+			yArray += (""+list2.get(i));
+			if(i != list2.size()-1) {
+				yArray += ", ";
+			}else {
+				yArray += "]"; 
+			}
+		}
+		
+		String nameArray = "[";
+		List<Object> nameList = nameColumn.getValues(nameColumn.getType());
+		for(int i = 0; i<nameList.size(); i++) {
+			if(i != nameList.size()-1) {
+				nameArray += ("'" + nameList.get(i) + "', ");
+			}else {
+				nameArray += ("'" + nameList.get(i) + "']");
+			}
+		}
+		
 
+		String html1 = "<html><head><script src=\"https://cdn.plot.ly/plotly-latest.min.js\"></script></head>";
+		String html2 = "<script type=\"text/javascript\" src=\"https://unpkg.com/react@16.2.0/umd/react.production.min.js\"></script>";
+		String html3 = "<script type=\"text/javascript\" src=\"https://unpkg.com/react-dom@16.2.0/umd/react-dom.production.min.js\"></script></head>";
+		String html4 = "<body><div id=\"scatterplot\" style=\"width:600px;height:600px;\"></div>";
+		String html5 = "<script> var trace1 = { x: " + xArray + ", y: " + yArray + ", type: 'scatter', mode: '" + this.getModeSelection() + "', text: " + nameArray + "};";
+		String html6 = "var trace2 = { x: " + xArray + ", y: " + yArray + ", type: 'scatter'};";
+		String html7 = "var data = [trace1];";
+		//String html8 = "var layout = { xaxis: {range: [0,5]}, yaxis: {range: [0,5]}};"; (code for manually setting the range, if I decide to do that.)
+		String html8 = "var layout = {hovermode: 'closest', title: 'Scatter Plot'};";
+		
+//		String html9 = "var myPlot = document.getElementById('scatterplot');";
+//		String html10 = "myPlot.on('plotly_click', function(data){ var pts = '';";
+//		String html11 = "for(var i=0; i<data.points.length; i++) {";
+//		String html12 = "pts = 'x= ' +data.points[i].x + '\ny = ' + data.points[i].y.toPrecision(4) + '\n\n';}";
+//		String html13 = "alert('Closest point clicked:\n\n'+pts);});";
+		
+		String html9 = "Plotly.react('scatterplot', data, layout);";
+		String html10 = "</script></body></html>";
+		
+        String html = html1 + html2 + html3 + html4 + html5 + html6 + html7 + html8 + html9 + html10;
+		Map<String, Object> args = new HashMap<>();
+		
+		args.put("text", html);
+		args.put("title", "Plot");
+		args.put("id", "01");
+		
 		//JFrame
-				JFrame fr = new JFrame("volcano plot interface");
-		        VolcanoPlotScreen sc = new VolcanoPlotScreen();
-		        fr.add(sc);
-		        fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		        fr.pack();
-		        fr.setVisible(true);
+//		JFrame fr = new JFrame("Scatter plot interface");
+//        ScatterPlotScreen sc = new ScatterPlotScreen();
+//        fr.add(sc);
+//        fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        fr.pack();
+//        fr.setVisible(true);
+		//
 		
 		TaskIterator ti = taskFactory.createTaskIterator("cybrowser", "show", args, null);
 		sTM.execute(ti);
 	}
-	
-	
 }
