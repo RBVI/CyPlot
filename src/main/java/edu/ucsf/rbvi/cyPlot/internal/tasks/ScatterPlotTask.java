1610b201c1e46f5e5b73871185f6d4ea4bf6336b
liammagee <lpdmagee@gmail.com>
2018-07-18 14:18:00 -0700

    Fixed JSUtils bug [scatter selection wasn't working]

--- src/main/java/edu/ucsf/rbvi/cyPlot/internal/tasks/ScatterPlotTask.java
+++ src/main/java/edu/ucsf/rbvi/cyPlot/internal/tasks/ScatterPlotTask.java
@@ -5,6 +5,9 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import javax.swing.JFrame;
+
 import org.cytoscape.application.CyApplicationManager;
 import org.cytoscape.command.CommandExecutorTaskFactory;
 import org.cytoscape.service.util.CyServiceRegistrar;
@@ -89,6 +92,26 @@ public class ScatterPlotTask extends AbstractTask {
 		items.put("mode",ModelUtils.getTunableSelection(mode));
 		items.put("nameSelection",ModelUtils.getTunableSelection(nameCol));
 		items.put("nameArray", nameArray);
+
+//		String html = "<html><head><script src=\"https://cdn.plot.ly/plotly-latest.min.js\"></script></head>";
+//		html += "<script type=\"text/javascript\" src=\"https://unpkg.com/react@16.2.0/umd/react.production.min.js\"></script>";
+//		html += "<script type=\"text/javascript\" src=\"https://unpkg.com/react-dom@16.2.0/umd/react-dom.production.min.js\"></script></head>";
+//		html += "<body><div id=\"scatterplot\" style=\"width:600px;height:600px;\"></div>";
+//		html +=  "<script> var trace1 = { x: " + items.get("x") + ", y: " + items.get("y") + ", type: 'scatter', mode: '" + items.get("mode") + "', text: " + items.get("nameArray") + "};";
+//		html += "var data = [trace1];";
+//		html += "var layout = {hovermode: 'closest', title: 'Scatter Plot'};";
+//		html += "Plotly.newPlot('scatterplot', data, layout);";
+//		html += "var myPlot = document.getElementById('scatterplot');";
+//		//on click stuff
+//		html += "myPlot.on('plotly_click', function(data){ \n ;";
+//		html += "cybrowser.executeCyCommand('network select nodeList = \"" + items.get("nameSelection") + ":' +data.points[0].text+'\"');});";
+//		//lasso and box stuff
+//		html += "myPlot.on('plotly_selected', function(data) { \n ;";
+//		html += "var nodelist = ''; for(var i = 0; i<data.points.length; i++) { nodelist+= (', "+items.get("nameSelection")+ ":' +data.points[i].text);};";
+//		html += "cybrowser.executeCyCommand('network select nodeList = \"'+nodelist+'\"');});";
+//		//important: always keep at bottom of HTML
+//		html += "Plotly.react();";
+//		html += "</script></body></html>";
 		
         String html = JSUtils.getHTML(items);
 		Map<String, Object> args = new HashMap<>();		
@@ -96,13 +119,11 @@ public class ScatterPlotTask extends AbstractTask {
 		args.put("title", "Plot");
 		
 		//JFrame if we want to add deeper customization options
-//		JFrame fr = new JFrame("Scatter plot interface");
+//		JFrame fr = new JFrame("Scatter plot control panel");
 //        ScatterPlotScreen sc = new ScatterPlotScreen();
 //        fr.add(sc);
-//        fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 //        fr.pack();
 //        fr.setVisible(true);
-		//
 		
 		TaskIterator ti = taskFactory.createTaskIterator("cybrowser", "show", args, null);
 		sTM.execute(ti);
