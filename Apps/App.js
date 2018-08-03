63ee3f1e5f6ad3d2b95aaef116f4708e3c05d941
Scooter Morris <scooter@cgl.ucsf.edu>
2018-08-03 13:53:24 -0700

    Added apps temporarily

---
+++ Apps/App.js
@@ -0,0 +1,48 @@
+import React, {Component} from 'react';
+import plotly from 'plotly.js/dist/plotly';
+import PlotlyEditor from 'react-chart-editor';
+import 'react-chart-editor/lib/react-chart-editor.css';
+
+const dataSources = {
+  cy1: [1, 2, 3], // eslint-disable-line no-magic-numbers
+  cy2: [4, 3, 2], // eslint-disable-line no-magic-numbers
+  cy3: [17, 13, 9], // eslint-disable-line no-magic-numbers
+};
+
+const dataSourceOptions = Object.keys(dataSources).map(name => ({
+  value: name,
+  label: name,
+}));
+
+const config = {editable: true};
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {data: [], layout: {}, frames: []};
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <PlotlyEditor
+          data={this.state.data}
+          layout={this.state.layout}
+          config={config}
+          frames={this.state.frames}
+          dataSources={dataSources}
+          dataSourceOptions={dataSourceOptions}
+          plotly={plotly}
+          onUpdate={(data, layout, frames) =>
+            this.setState({data, layout, frames})
+          }
+          useResizeHandler
+          debug
+          advancedTraceTypeSelector
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
