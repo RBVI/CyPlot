34ebba8dc764d75cd47500b949f7bb857b05bd91
Scooter Morris <scooter@cgl.ucsf.edu>
2019-01-07 09:49:27 -0800

    Oops -- forgot to add this

---
+++ src/main/java/edu/ucsf/rbvi/cyPlot/internal/tasks/CommandTunables.java
@@ -0,0 +1,29 @@
+package edu.ucsf.rbvi.cyPlot.internal.tasks;
+
+import org.cytoscape.work.Tunable;
+import org.cytoscape.work.util.ListSingleSelection;
+
+public class CommandTunables {
+	
+	@Tunable (description="Open in plot editor?")
+	public boolean editor;
+
+	@Tunable (description="Selection string", context="nogui")
+	public String selectionString = null;
+
+	@Tunable (description="Plot title", context="nogui")
+	public String title = null;
+
+	@Tunable (description="CyBrowser id", context="nogui")
+	public String id = null;
+
+	@Tunable (description="CyBrowser tab id", context="nogui")
+	public String tabID = null;
+
+	@Tunable (description="X Axis Label", context="nogui")
+	public String xLabel = null;
+
+	@Tunable (description="Y Axis Label", context="nogui")
+	public String yLabel = null;
+
+}
