c447bc04d03f32f5bca3784bc8fe760fa6175f36
Kartikkp07 <80201555+Kartikkp07@users.noreply.github.com>
2023-06-09 13:19:51 +0530

    Added ColumnTaskFactory files for respective plots
    For the implementation of Cyplot tools availability on right click of data column

---
+++ src/main/java/edu/ucsf/rbvi/cyPlot/internal/tasks/HeatMapColumnTaskFactory.java
@@ -0,0 +1,27 @@
+package edu.ucsf.rbvi.cyPlot.internal.tasks;
+
+import java.util.List;
+import java.util.Map;
+
+
+//import edu.ucsf.rbvi.cyPlot.internal.columnTasks.ScatterFilterTask;
+import edu.ucsf.rbvi.cyPlot.internal.tasks.ScatterPlotTask;
+import org.cytoscape.task.AbstractTableColumnTaskFactory;
+import org.cytoscape.model.CyColumn;
+import org.cytoscape.service.util.CyServiceRegistrar;
+import org.cytoscape.work.TaskIterator;
+
+public class HeatMapColumnTaskFactory extends AbstractTableColumnTaskFactory {
+//	final CyChartManager manager;
+	final CyServiceRegistrar cs_registrar;
+	
+	public HeatMapColumnTaskFactory(CyServiceRegistrar sr) {	cs_registrar = sr;	}
+	
+	public boolean isReady() {		return true;	}
+	
+	@Override
+	public TaskIterator createTaskIterator(CyColumn column)
+	{ 
+		return new TaskIterator(new HeatMapTask(cs_registrar)); 	
+	}
+}
